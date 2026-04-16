5047ea2f41e7ee2d97b5732a9fdf64e9cf4bed81
Jose-Maria-Caracuel <josemariacaracuelmenacho@gmail.com>
2026-04-16 18:59:35 +0200

    login y libreria

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { UsuarioPage } from './pages/usuario/usuario';
 import { LoginComponent } from './pages/login/login';
 import { ComentarioPage } from './pages/Comentarios/Comentario';
+import { MiLibreria } from './pages/mi_libreria/mi_libreria';
 
 export const routes: Routes = [
 	{ path: '', component: Home },
 	{ path: 'comentarios/:id', component: ComentarioPage },
 	{ path: 'usuario', component: UsuarioPage },
 	{ path: 'login', component: LoginComponent },
+	{ path: 'mi_libreria', component: MiLibreria },
 	{ path: '**', redirectTo: '' }
 ];

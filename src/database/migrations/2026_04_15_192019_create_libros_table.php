b40f8fb61de1df04e22d596555d599b6a223723c
Francisco Manuel <franciscomanueltoledo@gmail.com>
2026-04-20 12:03:41 +0200

    muñecos de trapo

--- src/database/migrations/2026_04_15_192019_create_libros_table.php
+++ src/database/migrations/2026_04_15_192019_create_libros_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('libros')) {
+            return;
+        }
+
         Schema::create('libros', function (Blueprint $table) {
             $table->id();
             $table->string('titulo')->nullable();

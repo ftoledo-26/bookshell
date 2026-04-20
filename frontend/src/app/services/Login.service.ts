fe073922a7bbceca80fb960a5941f79b9958afa6
Francisco Manuel <franciscomanueltoledo@gmail.com>
2026-04-20 10:24:18 +0200

    crear y loger

--- frontend/src/app/services/Login.service.ts
+++ frontend/src/app/services/Login.service.ts
@@ -6,14 +6,16 @@ import {tap} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class LoginService {
-    private apiUrl = environment.API_URL+'/login';
+    private apiUrl = environment.API_URL+'login';
 
     constructor(private http: HttpClient, private router: Router) {}
 
     login(email: string, password: string) {
-        return this.http.post<{ token: string}>(this.apiUrl, { email, password }).pipe(
+        return this.http.post<{ token: string, user: { username: string, id: number , foto: string} }>(this.apiUrl, { email, password }).pipe(
             tap(response => {
-                localStorage.setItem('token', response.token);
+                localStorage.setItem('token', response['token']);
+                localStorage.setItem('username', response['user']['username']);
+                localStorage.setItem('foto', response['user']['foto']);
                 this.router.navigate(['/']);
             }
         ));

59b7ada7ebc89df2f94081b9c31fcc62553deb06
JesusMoreno05 <morenobernaljesus@gmail.com>
2026-04-21 17:27:53 +0200

    funciones varias

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibrosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/libros/{id}/comentarios', [LibrosController::class, 'comentarios'])->name('libros.comentarios');
+
+Route::get('/libros/top-rated', [LibrosController::class, 'topRated'])->name('libros.topRated');

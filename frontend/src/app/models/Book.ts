ed52ecd85736838ad2bfdd1b2bf92f2864129eee
Francisco Manuel <franciscomanueltoledo@gmail.com>
2026-04-30 12:34:32 +0200

    subida a Back

--- frontend/src/app/models/Book.ts
+++
@@ -1,7 +0,0 @@
-export interface Book {
-    id: number;
-    titulo: string;
-    autor: string;
-    descripcion: string;
-    portada: string;
-}
--- frontend/src/app/models/Usuario.ts
+++
@@ -1,9 +0,0 @@
-export interface Usuario {
-    id: number;
-    nombre: string;
-    email: string;
-    password: string;
-    rol: string;
-    foto: string;
-    phone: string;
-}

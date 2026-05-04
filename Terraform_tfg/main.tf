d403cb299fa60cb6e30817dcf780f2906edf91f4
Francisco Manuel <franciscomanueltoledo@gmail.com>
2026-05-04 12:26:06 +0200

    fix: backend S3 para tfstate y deshabilitar trigger automatico de terraform

--- Terraform_tfg/main.tf
+++ Terraform_tfg/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "bookshell-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {

b284deee55d74ecc5b185937879f569c03cf0283
JesusMoreno05 <morenobernaljesus@gmail.com>
2026-05-08 13:20:26 +0200

    safe to say im the biggest, the largest

---
+++ src/app/Filament/Admin/Resources/LibroResource/Pages/CreateLibro.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\LibroResource\Pages;
+
+use App\Filament\Admin\Resources\LibroResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateLibro extends CreateRecord
+{
+    protected static string $resource = LibroResource::class;
+}
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->roll === 'admin';
+    }
 
     /**
      * The attributes that are mass assignable.

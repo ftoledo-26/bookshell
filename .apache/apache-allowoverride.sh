a2ff8e565be0e4a8416b6cbf5c1b7afcd9554aa8
Francisco Manuel <franciscomanueltoledo@gmail.com>
2026-05-12 14:47:47 +0200

    Revert "fix: eliminar heredoc del workflow que causaba error YAML L102"
    This reverts commit d9b0259cb9ca63f4013e0b7e5ce31308126c2360.

--- .apache/apache-allowoverride.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp /etc/letsencrypt/renewal-hooks/deploy/bookshell-ssl.conf \
-  /etc/apache2/sites-available/000-default-le-ssl.conf
-cp /var/www/back/.apache/laravel.conf /etc/apache2/conf-available/laravel.conf
-systemctl reload apache2

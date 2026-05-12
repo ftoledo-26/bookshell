dc29c6871cf5b610c5a9ab44ae147f558c3e89ce
Francisco Manuel <franciscomanueltoledo@gmail.com>
2026-05-12 14:12:46 +0200

    feat: actualizar ADMIN_URL a /filament tras cambio de ruta en Filament

--- frontend/src/app/environments/environments.ts
+++ frontend/src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   API_URL: "https://bookshell.duckdns.org/api/",
-  ADMIN_URL: "https://bookshell.duckdns.org/admin"
+  ADMIN_URL: "https://bookshell.duckdns.org/filament"
 };
